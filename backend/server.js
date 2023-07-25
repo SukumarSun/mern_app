ef29b24aa942e1ac71a68ecba99e5b5ecdd52df5
sukumar <msukumarsundar12@gmail.com>
2023-07-25 16:17:38 +0530

    before redux login and register functionality

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
+const { notFound, errorHandler } = require("./middlewares/errormiddleware");
+//
 
 const app = express();
-dotenv.config();
+dotenv.config({ path: "./../.env" });
+connectDB();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("get is running");
@@ -14,11 +19,22 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  const { id } = req.params;
-  const note = notes.find((each) => each._id === id);
-  res.send(note);
-});
+app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, console.log(`Server is running on ${PORT}`));
+app.use(notFound);
+app.use(errorHandler);
+
+// app.get("/api/notes/:id", (req, res) => {
+//   const { id } = req.params;
+//   const note = notes.find((each) => each._id === id);
+//   res.send(note);
+// });
+
+const PORT = process.env.PORT || 3001;
+// app.listen(PORT, console.log(`Server is running on ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+  // console.log(process.env.PORT);
+  // console.log(dotenv.config());
+});
